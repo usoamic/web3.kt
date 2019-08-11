3551af023fb954b04c099f8caaf511063db50410
primys <a2uso@tuta.io>
2019-08-11 08:09:55 -0700

    Remove redundant code

--- src/main/kotlin/io/usoamic/webwallet/main.kt
+++ src/main/kotlin/io/usoamic/webwallet/main.kt
@@ -3,13 +3,7 @@ package io.usoamic.webwallet
 import js.externals.jquery.jQuery
 
 fun setResult(str: String) {
-    require("toastr")
     jQuery("#result").text(str)
-}
-
-@JsModule("toastr")
-external class toastr {
-
 }
 
 fun main() {
