e9f0ad77a1fd59eb6f84054296924170763f2052
primys <a2uso@tuta.io>
2019-08-17 08:22:09 -0700

    Replace Double and Int to BigNumber

--- src/main/kotlin/io/usoamic/web3kt/model/Transaction.kt
+++ src/main/kotlin/io/usoamic/web3kt/model/Transaction.kt
@@ -1,19 +1,21 @@
 package io.usoamic.web3kt.model
 
+import io.usoamic.web3kt.BigNumber
+
 class Transaction private constructor(
     val from: String?,
-    val nonce: Int? /* BigInteger */,
-    val gasPrice: Int? /* BigInteger */,
-    val gasLimit: Int? /* BigInteger */,
+    val nonce: BigNumber? /* BigInteger */,
+    val gasPrice: BigNumber? /* BigInteger */,
+    val gasLimit: BigNumber? /* BigInteger */,
     val to: String?,
-    val value: Int? /* BigInteger */,
+    val value: BigNumber? /* BigInteger */,
     val data: String?
 ) {
     companion object {
         fun createEthCallTransaction(from: String, to: String, data: String) =
             Transaction(from, null, null, null, to, null, data)
 
-        fun createTransaction(from: String?, nonce: Int?, gasPrice: Int?, gasLimit: Int?, to: String?, value: Int?, data: String?) =
+        fun createTransaction(from: String?, nonce: BigNumber?, gasPrice: BigNumber?, gasLimit: BigNumber?, to: String?, value: BigNumber?, data: String?) =
             Transaction(from, nonce, gasPrice, gasLimit, to, value, data)
     }
 }
--- src/main/kotlin/io/usoamic/web3kt/web3/Utils.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/Utils.kt
@@ -1,9 +1,11 @@
 package io.usoamic.web3kt.web3
 
+import io.usoamic.web3kt.BigNumber
+
 external class Utils {
     fun checkAddressChecksum(address: String): Boolean
     fun toChecksumAddress(address: String): String
     fun isAddress(address: String): Boolean
-    fun fromWei(value: String, unit: String): Double
-    fun toWei(number: String, unit: String): Double
+    fun fromWei(value: String, unit: String): BigNumber
+    fun toWei(number: String, unit: String): BigNumber
 }
