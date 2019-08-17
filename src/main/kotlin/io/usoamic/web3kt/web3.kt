d78f1624b3bafe7c196c2782b982648e37f0015f
primys <a2uso@tuta.io>
2019-08-17 02:30:22 -0700

    Add various functions of web3.js

--- src/main/kotlin/io/usoamic/web3kt/web3.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3.kt
@@ -3,12 +3,24 @@ package io.usoamic.web3kt
 @JsModule("web3")
 external class Web3(provider: String) {
     var utils: Utils = definedExternally
+    var eth: Eth = definedExternally
 
     companion object {
         var utils: Utils = definedExternally
     }
 }
 
+external class Eth {
+    fun getBalance(address: String, defaultBlock: String, callback: (error: String?, balance: String?) -> Unit)
+    fun getTransactionCount(address: String, defaultBlock: String, callback: (error: String?, balance: String?) -> Unit)
+    fun getBlockNumber(callback: (error: String?, number: String?) -> Unit)
+    fun estimateGas(`object`: Any, callback: (error: String?, gas: String?) -> Unit): String
+}
+
 external class Utils {
+    fun checkAddressChecksum(address: String): Boolean
+    fun toChecksumAddress(address: String): String
+    fun isAddress(address: String): Boolean
     fun fromWei(value: String, unit: String): Double
+    fun toWei(number: String, unit: String): Double
 }
