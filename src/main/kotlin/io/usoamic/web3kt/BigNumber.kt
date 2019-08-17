9d468899f49c4ff4dbd02ef500b78d09ece35d22
primys <a2uso@tuta.io>
2019-08-17 08:23:28 -0700

    Add additional constructors

--- src/main/kotlin/io/usoamic/web3kt/BigNumber.kt
+++ src/main/kotlin/io/usoamic/web3kt/BigNumber.kt
@@ -2,6 +2,10 @@ package io.usoamic.web3kt
 
 @JsModule("bignumber.js")
 external class BigNumber(value: String) {
+    constructor(value: Int)
+    constructor(value: Double)
+    constructor(value: Long)
+
     fun isEqualTo(value: BigNumber): Boolean
     fun plus(value: BigNumber)
     fun minus(value: BigNumber)
