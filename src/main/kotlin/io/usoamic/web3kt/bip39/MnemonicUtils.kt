ae2764811361b6390c26249522144b045023c301
primys <a2uso@tuta.io>
2019-08-18 12:12:33 -0700

    Rename Bip39 to MnemonicUtils and add function mnemonicToSeed with password argument

---
+++ src/main/kotlin/io/usoamic/web3kt/bip39/MnemonicUtils.kt
@@ -0,0 +1,15 @@
+package io.usoamic.web3kt.bip39
+
+import io.usoamic.web3kt.buffer.Buffer
+
+@JsModule("bip39")
+external class MnemonicUtils {
+    companion object {
+        fun generateMnemonic(): String
+        fun isValidMnemonicPhrase(phrase: String)
+        @JsName("mnemonicToSeedSync")
+        fun mnemonicToSeed(phrase: String): Buffer
+        @JsName("mnemonicToSeedSync")
+        fun mnemonicToSeed(phrase: String, password: String): Buffer
+    }
+}
