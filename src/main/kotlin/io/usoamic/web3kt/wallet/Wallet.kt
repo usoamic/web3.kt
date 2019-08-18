b1b4bf0b54769304addbe43f123e113b939d3a37
primys <a2uso@tuta.io>
2019-08-18 07:42:08 -0700

    Move functions from WalletExtension to Wallet

--- src/main/kotlin/io/usoamic/web3kt/wallet/Wallet.kt
+++ src/main/kotlin/io/usoamic/web3kt/wallet/Wallet.kt
@@ -6,10 +6,16 @@ import io.usoamic.web3kt.Buffer
 open external class Wallet {
     fun getPublicKey(): Buffer
     fun getPrivateKey(): Buffer
+    @JsName("getPrivateKeyString")
+    fun getPrivateKeyAsString(): String
+    @JsName("getPublicKeyString")
+    fun getPublicKeyAsString(): String
     fun getChecksumAddressString(): String
     fun toV3(password: String): String
 
+
     companion object {
+        fun generate(): Wallet
         fun fromPrivateKey(privateKey: Buffer): Wallet
         fun fromV3(json: String, password: String): Wallet
     }
