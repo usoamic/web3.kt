03afba57fc2bc8d316277c82dc94a4b08aea8419
primys <a2uso@tuta.io>
2019-08-24 12:55:30 -0700

    Minor

--- src/main/kotlin/io/usoamic/web3kt/web3/extension/Web3Extension.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/extension/Web3Extension.kt
@@ -5,6 +5,8 @@ import io.usoamic.web3kt.web3.Web3
 import io.usoamic.web3kt.web3.contract.other.ContractWrapper
 import kotlin.js.Json
 
+inline fun func1() { }
+
 inline fun <T : ContractWrapper>Web3.newContract(jsonInterface: Json, address: String): Contract<T> {
     val web3 = this
     return js("new web3.eth.Contract(jsonInterface, address)").unsafeCast<Contract<T>>()
