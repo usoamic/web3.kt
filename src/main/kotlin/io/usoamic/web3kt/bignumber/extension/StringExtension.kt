90c0cb622f16588e15f3bcbf03236555657b0dce
primys <a2uso@tuta.io>
2019-08-24 12:51:41 -0700

    Remove inline prefix

--- src/main/kotlin/io/usoamic/web3kt/bignumber/extension/StringExtension.kt
+++ src/main/kotlin/io/usoamic/web3kt/bignumber/extension/StringExtension.kt
@@ -2,6 +2,6 @@ package io.usoamic.web3kt.bignumber.extension
 
 import io.usoamic.web3kt.bignumber.BigNumber
 
-inline fun String.toBigNumber(): BigNumber {
+fun String.toBigNumber(): BigNumber {
     return BigNumber(this)
 }
