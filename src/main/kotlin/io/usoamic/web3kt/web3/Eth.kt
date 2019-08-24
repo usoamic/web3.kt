b23afa411471c854fe3459956f9723ee6d350ace
primys <a2uso@tuta.io>
2019-08-24 12:55:12 -0700

    Add Promise type of return to sendSignedTransaction function

--- src/main/kotlin/io/usoamic/web3kt/web3/Eth.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/Eth.kt
@@ -1,9 +1,11 @@
 package io.usoamic.web3kt.web3
 
+import kotlin.js.Promise
+
 external interface Eth {
     fun getBalance(address: String, defaultBlock: String, callback: (error: String?, balance: String?) -> Unit)
     fun getTransactionCount(address: String, defaultBlock: String, callback: (error: String?, balance: String?) -> Unit)
     fun getBlockNumber(callback: (error: String?, number: String?) -> Unit)
     fun estimateGas(`object`: Any, callback: (error: String?, gas: String?) -> Unit): String
-    fun sendSignedTransaction(sendSignedTransaction: String)
+    fun sendSignedTransaction(signedTransaction: String) : Promise<String>
 }
