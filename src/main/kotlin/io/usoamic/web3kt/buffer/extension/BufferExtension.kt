ebec17494eba125d23f2ad76dc077914cbcb57db
primys <a2uso@tuta.io>
2019-08-24 12:52:00 -0700

    Add fromHex() method

--- src/main/kotlin/io/usoamic/web3kt/buffer/extension/BufferExtension.kt
+++ src/main/kotlin/io/usoamic/web3kt/buffer/extension/BufferExtension.kt
@@ -5,3 +5,7 @@ import io.usoamic.web3kt.buffer.Buffer
 inline fun Buffer.toHex(): String {
     return this.toString("hex")
 }
+
+inline fun Buffer.Companion.fromHex(input: String): Buffer {
+    return Buffer.from(input, "hex")
+}
