5c61078f97f436028ab3bfef3ce9be8929882ca9
primys <a2uso@tuta.io>
2019-08-24 12:53:23 -0700

    Add encodeABI() function and replace callback for Promise

--- src/main/kotlin/io/usoamic/web3kt/web3/contract/unit/ExecuteUnit.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/contract/unit/ExecuteUnit.kt
@@ -3,10 +3,13 @@ package io.usoamic.web3kt.web3.contract.unit
 import io.usoamic.web3kt.bignumber.BigNumber
 import io.usoamic.web3kt.web3.contract.model.EstimateGasOption
 import io.usoamic.web3kt.web3.contract.model.ExecuteOption
+import kotlin.js.Promise
 
 interface ExecuteUnit {
     @JsName("send")
-    fun execute(executeOption: ExecuteOption)
+    fun execute(executeOption: ExecuteOption, callback: (error: String?, result: BigNumber) -> Unit)
     @JsName("estimateGas")
-    fun estimateGas(estimateGasOption: EstimateGasOption, callback: (error: String?, result: BigNumber) -> Unit)
+    fun estimateGas(estimateGasOption: EstimateGasOption): Promise<String>
+    @JsName("encodeABI")
+    fun encodeABI(): String
 }
