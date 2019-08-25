5eaa647df8984f20b90197a5d3b5df0e6c586d0b
primys <a2uso@tuta.io>
2019-08-25 00:09:43 -0700

    Add arguments to and data to EstimateGasOption class

--- src/main/kotlin/io/usoamic/web3kt/web3/contract/model/EstimateGasOption.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/contract/model/EstimateGasOption.kt
@@ -1,8 +1,11 @@
 package io.usoamic.web3kt.web3.contract.model
 
 import io.usoamic.web3kt.bignumber.BigNumber
+import io.usoamic.web3kt.bignumber.BigNumberValue
 
 data class EstimateGasOption (
     val from: String,
-    val value: BigNumber = BigNumber(0)
+    val to: String,
+    val value: BigNumber = BigNumberValue.ZERO,
+    val data: String = ""
 )
