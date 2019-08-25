67fda82db6be76f877011dd21783698b4fdcb373
primys <a2uso@tuta.io>
2019-08-25 12:01:13 -0700

    Add Unit

---
+++ src/main/kotlin/io/usoamic/web3kt/util/Unit.kt
@@ -0,0 +1,16 @@
+package io.usoamic.web3kt.util
+
+class Unit {
+    companion object {
+        val WEI = "wei"
+        val KWEI = "kwei"
+        val MWEI = "mwei"
+        val GWEI = "gwei"
+        val SZABO = "szabo"
+        val FINNEY = "finney"
+        val ETHER = "ether"
+        val KETHER = "kether"
+        val METHER = "mether"
+        val GETHER = "gether"
+    }
+}
