f82a477747c313f9297b8f1bec5ee6065c1ca468
primys <a2uso@tuta.io>
2019-08-25 00:03:38 -0700

    Add getGasPrice method and sendSignedTransaction now returns TransactionReceipt

--- src/main/kotlin/io/usoamic/web3kt/web3/Eth.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/Eth.kt
@@ -1,5 +1,8 @@
 package io.usoamic.web3kt.web3
 
+import io.usoamic.web3kt.bignumber.BigNumber
+import io.usoamic.web3kt.buffer.Buffer
+import io.usoamic.web3kt.tx.model.TransactionReceipt
 import kotlin.js.Promise
 
 external interface Eth {
@@ -7,5 +10,6 @@ external interface Eth {
     fun getTransactionCount(address: String, defaultBlock: String, callback: (error: String?, balance: String?) -> Unit)
     fun getBlockNumber(callback: (error: String?, number: String?) -> Unit)
     fun estimateGas(`object`: Any, callback: (error: String?, gas: String?) -> Unit): String
-    fun sendSignedTransaction(signedTransaction: String) : Promise<String>
+    fun sendSignedTransaction(signedTransaction: String) : Promise<TransactionReceipt>
+    fun getGasPrice(): Promise<BigNumber>
 }
