c8ee27ad9220f445db52c84855d9d031e6623e34
primys <a2uso@tuta.io>
2019-08-25 00:04:03 -0700

    Add toHex method and rename arguments

--- src/main/kotlin/io/usoamic/web3kt/web3/Utils.kt
+++ src/main/kotlin/io/usoamic/web3kt/web3/Utils.kt
@@ -1,6 +1,7 @@
 package io.usoamic.web3kt.web3
 
 import io.usoamic.web3kt.bignumber.BigNumber
+import io.usoamic.web3kt.buffer.Buffer
 
 external interface Utils {
     fun checkAddressChecksum(address: String): Boolean
@@ -10,8 +11,10 @@ external interface Utils {
     fun fromWei(value: BigNumber, unit: String): String
     fun fromWei(value: Int, unit: String): String
     fun fromWei(value: Long, unit: String): String
-    fun toWei(number: String, unit: String): String
-    fun toWei(number: BigNumber, unit: String): String
-    fun toWei(number: Int, unit: String): String
-    fun toWei(number: Long, unit: String): String
+    fun toWei(value: String, unit: String): String
+    fun toWei(value: BigNumber, unit: String): String
+    fun toWei(value: Int, unit: String): String
+    fun toWei(value: Long, unit: String): String
+    fun toHex(value: BigNumber): Buffer
+    fun toHex(value: String): Buffer
 }
