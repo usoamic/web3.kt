5249eb4f138289a883a0d760d6f7be1dcc3320ea
primys <a2uso@tuta.io>
2019-08-25 00:08:22 -0700

    Add method addHexPrefix() and rename removePrefix() to removeHexPrefix()

--- src/main/kotlin/io/usoamic/web3kt/util/extension/UtilsExtension.kt
+++ src/main/kotlin/io/usoamic/web3kt/util/extension/UtilsExtension.kt
@@ -1,5 +1,10 @@
 package io.usoamic.web3kt.util.extension
 
-fun String.removePrefix(): String {
+fun String.removeHexPrefix(): String {
     return this.substring(2)
 }
+
+fun String.addHexPrefix(): String {
+    return "0x$this"
+}
+
