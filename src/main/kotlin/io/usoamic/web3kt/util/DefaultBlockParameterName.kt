2d8713b76b620b990268cc69780a1e997db0f353
primys <a2uso@tuta.io>
2019-08-25 00:10:02 -0700

    Add DefaultBlockParameterName class

---
+++ src/main/kotlin/io/usoamic/web3kt/util/DefaultBlockParameterName.kt
@@ -0,0 +1,9 @@
+package io.usoamic.web3kt.util
+
+class DefaultBlockParameterName {
+    companion object {
+        val EARLIEST = "earliest"
+        val LATEST = "latest"
+        val PENDING = "pending"
+    }
+}
