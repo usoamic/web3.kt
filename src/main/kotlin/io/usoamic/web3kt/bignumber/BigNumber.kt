9a2b00314feaa47c303b13a75e692316f918f11a
primys <a2uso@tuta.io>
2019-08-25 00:06:37 -0700

    Add function for comparison, pow method and rename times -> multipliedBy, also add constructor for BigNumber

--- src/main/kotlin/io/usoamic/web3kt/bignumber/BigNumber.kt
+++ src/main/kotlin/io/usoamic/web3kt/bignumber/BigNumber.kt
@@ -1,19 +1,29 @@
 package io.usoamic.web3kt.bignumber
 
+import io.usoamic.web3kt.buffer.Buffer
+
 @JsModule("bignumber.js")
 external class BigNumber(value: String) {
     constructor(value: Int)
     constructor(value: Double)
     constructor(value: Long)
+    constructor(value: BigNumber)
 
     fun isEqualTo(value: BigNumber): Boolean
-    fun plus(value: BigNumber)
-    fun minus(value: BigNumber)
+    fun isGreaterThan(value: BigNumber): Boolean
+    fun isLessThan(value: BigNumber): Boolean
+    fun isLessThanOrEqualTo(value: BigNumber): Boolean
+    fun isGreaterThanOrEqualTo(value: BigNumber): Boolean
+    fun isZero(value: BigNumber): Boolean
+    fun plus(value: BigNumber): BigNumber
+    fun minus(value: BigNumber): BigNumber
     fun toFixed(dp: Int): BigNumber
     fun dividedBy(value: BigNumber): BigNumber
-    fun times(value: BigNumber): BigNumber
+    @JsName("multipliedBy")
+    fun multipliedBy(value: BigNumber): BigNumber
     fun toNumber(): BigNumber
     fun toFormat(dp: Int): BigNumber
+    fun pow(n: Int): BigNumber
 
     companion object {
         val ROUND_UP: Int
