d6ddffb6b37d64d30fcf1b01040439dc4d6bc94e
primys <a2uso@tuta.io>
2019-08-25 00:07:49 -0700

    Rename Tx to Transaction

--- src/main/kotlin/io/usoamic/web3kt/tx/Transaction.kt
+++ src/main/kotlin/io/usoamic/web3kt/tx/Transaction.kt
@@ -1,9 +1,10 @@
 package io.usoamic.web3kt.tx
 
 import io.usoamic.web3kt.buffer.Buffer
+import io.usoamic.web3kt.tx.model.RawTransaction
 
 @JsModule("ethereumjs-tx")
-external class Tx(tx: Any) {
+external class Transaction(tx: RawTransaction) {
     fun sign(privateKey: Buffer)
-    fun serialize(): String
+    fun serialize(): Buffer
 }
