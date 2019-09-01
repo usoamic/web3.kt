6ad1ec6f4711c9bc324374db1dcd73893f7bcd48
primys <a2uso@tuta.io>
2019-09-01 07:31:55 -0700

    Add hasPrefix() extension

--- src/main/kotlin/io/usoamic/web3kt/util/extension/UtilsExtension.kt
+++ src/main/kotlin/io/usoamic/web3kt/util/extension/UtilsExtension.kt
@@ -1,9 +1,15 @@
 package io.usoamic.web3kt.util.extension
 
+import kotlin.math.min
+
 fun String.removeHexPrefix(): String {
     return this.substring(2)
 }
 
 fun String.addHexPrefix(): String {
     return "0x$this"
+}
+
+fun String.hasPrefix(): Boolean {
+    return (substring(0, min(this.length, 2)) == "0x")
 }
