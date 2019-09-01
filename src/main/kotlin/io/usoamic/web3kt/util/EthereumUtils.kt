73a212605f499ef67cbdf451e96b4dcbd48430ae
primys <a2uso@tuta.io>
2019-09-01 01:53:52 -0700

    Move functions to companion object

--- src/main/kotlin/io/usoamic/web3kt/util/EthereumUtils.kt
+++ src/main/kotlin/io/usoamic/web3kt/util/EthereumUtils.kt
@@ -5,11 +5,13 @@ import io.usoamic.web3kt.buffer.extension.fromHex
 import io.usoamic.web3kt.util.module.EthUtils
 
 class EthereumUtils {
-    fun isValidPrivateKey(privateKey: String): Boolean {
-        return EthUtils.isValidPrivateKey(Buffer.fromHex(privateKey))
-    }
+    companion object {
+        fun isValidPrivateKey(privateKey: String): Boolean {
+            return EthUtils.isValidPrivateKey(Buffer.fromHex(privateKey))
+        }
 
-    fun isValidPrivateKey(privateKey: Buffer): Boolean {
-        return EthUtils.isValidPrivateKey(privateKey)
+        fun isValidPrivateKey(privateKey: Buffer): Boolean {
+            return EthUtils.isValidPrivateKey(privateKey)
+        }
     }
 }
