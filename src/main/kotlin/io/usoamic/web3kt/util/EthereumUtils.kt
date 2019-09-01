759982db887424501a0c6be83fa0ff6e4cedf62e
primys <a2uso@tuta.io>
2019-09-01 01:59:15 -0700

    Add function isValidAddress

--- src/main/kotlin/io/usoamic/web3kt/util/EthereumUtils.kt
+++ src/main/kotlin/io/usoamic/web3kt/util/EthereumUtils.kt
@@ -2,6 +2,7 @@ package io.usoamic.web3kt.util
 
 import io.usoamic.web3kt.buffer.Buffer
 import io.usoamic.web3kt.buffer.extension.fromHex
+import io.usoamic.web3kt.core.Web3
 import io.usoamic.web3kt.util.module.EthUtils
 
 class EthereumUtils {
@@ -12,6 +13,10 @@ class EthereumUtils {
 
         fun isValidPrivateKey(privateKey: Buffer): Boolean {
             return EthUtils.isValidPrivateKey(privateKey)
+        }
+
+        fun isValidAddress(address: String): Boolean {
+            return Web3.utils.isAddress(address)
         }
     }
 }
