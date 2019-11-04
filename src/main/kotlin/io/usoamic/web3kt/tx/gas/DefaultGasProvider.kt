d67933b74d146b25f5466abc858390d033b4e4d5
primys <a2uso@tuta.io>
2019-11-03 16:40:32 -0800

    Minor changes to fix issue

--- src/main/kotlin/io/usoamic/web3kt/tx/gas/DefaultGasProvider.kt
+++ src/main/kotlin/io/usoamic/web3kt/tx/gas/DefaultGasProvider.kt
@@ -4,7 +4,7 @@ import io.usoamic.web3kt.bignumber.BigNumber
 
 class DefaultGasProvider {
     companion object {
-        val GAS_LIMIT = BigNumber(4300000)
-        val GAS_PRICE = BigNumber(22_000_000_000L)
+        val GAS_LIMIT: BigNumber = BigNumber(4_300_000L).toNumber()
+        val GAS_PRICE: BigNumber = BigNumber(22_000_000_000L).toNumber()
     }
 }
