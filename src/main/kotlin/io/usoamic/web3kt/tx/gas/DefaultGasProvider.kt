ac696a1a0d00810b652b33c3106d01b73e5547d0
primys <a2uso@tuta.io>
2020-06-16 15:38:26 -0700

    Add additional gas prices

--- src/main/kotlin/io/usoamic/web3kt/tx/gas/DefaultGasProvider.kt
+++ src/main/kotlin/io/usoamic/web3kt/tx/gas/DefaultGasProvider.kt
@@ -8,6 +8,9 @@ class DefaultGasProvider {
         val GAS_PRICE_20: BigNumber = BigNumber(20_000_000_000L).toNumber()
         val GAS_PRICE_35: BigNumber = BigNumber(35_000_000_000L).toNumber()
         val GAS_PRICE_50: BigNumber = BigNumber(50_000_000_000L).toNumber()
+        val GAS_PRICE_75: BigNumber = BigNumber(75_000_000_000L).toNumber()
+        val GAS_PRICE_100: BigNumber = BigNumber(100_000_000_000L).toNumber()
+        val GAS_PRICE_125: BigNumber = BigNumber(125_000_000_000L).toNumber()
         val DEFAULT_GAS_PRICE: BigNumber  = BigNumber(35_000_000_000L).toNumber()
     }
 }
